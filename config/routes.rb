658b008baaee9a842e11b3bf0b8f8ee600972a5c
Haji Mammadov <hmammado@gmail.com>
2021-11-11 17:30:35 +0100

    Movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
+  resources :lists do
+    resources :bookmarks, only: [:new, :create ]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
